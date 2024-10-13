83490ac851cd3d75c6f17519117d2dbff05aafbd
Steve Drasco <scd8@st-andrews.ac.uk>
2024-10-13 13:09:14 +0100

    Initial upload of modularized code

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+from .market_data_client import MarketDataClient
+from .strangle_finder import StrangleFinder
+from .report_generator import ReportGenerator
+from .models import Strangle
