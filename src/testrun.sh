3f8a34b4adb59fa709a3ba0d5c3a3db08e255aa1
sdrasco <steve.drasco@gmail.com>
2024-10-16 10:30:56 +0100

    removed redundant files

--- src/testrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# testrun.sh
-
-# Navigate to the src directory (adjust the path as needed)
-cd /Users/sdrasco/Desktop/ModularEdgeWalker/src
-
-# Run your main script with profiling (if desired)
-python3 main.py --profile
