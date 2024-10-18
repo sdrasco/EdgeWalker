f9489b5295d24753e11371a9eac39f2652a90ff5
sdrasco <steve.drasco@gmail.com>
2024-10-18 19:52:31 +0100

    async and main are merged, working well

--- src/main.py
+++ src/main.py
@@ -65,7 +65,7 @@ async def main():  # Make main an async function
     # You'll need to tune.  It's hard to know when you will break 
     # the API pull rate limit. Advice: start from 2 and build up.
     # you should also tune the batch size for the fetch_all_stock_prices.
-    concurrent_requests = 10
+    concurrent_requests = 3
     semaphore = asyncio.Semaphore(concurrent_requests) 
 
     # Initialize the MarketDataClient
