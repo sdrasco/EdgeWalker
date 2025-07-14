ffad6e061a4f51cdeabdc4c9f8095f981170e4a9
Steve Drasco <steve.drasco@gmail.com>
2025-07-14 16:09:51 +0100

    update report

--- src/strangle_finder.py
+++ src/strangle_finder.py
@@ -28,7 +28,7 @@ class StrangleFinder:
     async def find_balanced_strangle(self, ticker: str, semaphore=None) -> Optional[Strangle]:
         
         # Set date limits
-        date_min = datetime.today() + timedelta(days=20)
+        date_min = datetime.today() + timedelta(days=30)
         date_max = date_min + timedelta(days=180)
         date_min = date_min.strftime('%Y-%m-%d')
         date_max = date_max.strftime('%Y-%m-%d')
